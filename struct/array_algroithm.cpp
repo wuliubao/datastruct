331d2128cbf4e21f3877846a143ca22caa77764e
wuliubao <ahjxliubao@163.com>
2022-04-07 20:32:48 +0800

    delete useless

--- struct/array_algroithm.cpp
+++ struct/array_algroithm.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <algorithm >
+#include <algorithm>
+#include <regex>
 
 using namespace std;
 
@@ -44,6 +45,24 @@ void street_lamp_range() {
 
 	cout << out_v << endl;
 }
+
+//½»»»
+void min_swap() {
+	string input;
+	getline(cin, input);
+	regex split(",");
+	vector<int> pro_array;
+
+	sregex_token_iterator end;
+	for (sregex_token_iterator iter(input.begin(), input.end(), split, -1); iter != end; iter++) {
+		pro_array.push_back(stoi(iter->str()));
+	}
+
+	for (int item : pro_array) {
+		cout << item << endl;
+	}
+};
+
 void array_algroithm_test() {
 	street_lamp_range();
 }
--- struct/string_algorithm.cpp
+++ struct/string_algorithm.cpp
@@ -6,6 +6,35 @@
 
 using namespace std;
 
+int optPri(char opt) {
+	switch (opt) {
+	case '#':
+		return 1;
+		break;
+	case '$':
+		return 2;
+		break;
+	default:
+		break;
+	}
+}
+
+void alien_expression() {
+	string input;
+	cin >> input;
+
+	stack<int> numStack;
+	stack<char> optStack;
+
+	for (int i = 0; i < input.size(); i++) {
+		if (input[i] > '0' && input[i] < '9') {
+			numStack.push(input[i]);
+		}
+		if (input[i] == '#' || input[i] == 's') {
+		}
+	}
+}
+
 void bf(vector<size_t>& out, string& main, string& sub) {
 	size_t x = 0;
 	size_t y = 0;
