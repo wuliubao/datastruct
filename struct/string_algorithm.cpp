10d805dd1f87e8ea3a0d2315f5be371bc0bc46f6
wuliubao <ahjxliubao@163.com>
2023-08-08 22:35:41 +0800

    change code position

--- struct/string_algorithm.cpp
+++ struct/string_algorithm.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <regex>
 #include <stack>
 #include <set>
+#include <unordered_map>
 
 using namespace std;
 
@@ -94,6 +95,44 @@ void string_algroithm_test() {
 
 	//cout << out.size() << endl;
 	for (auto item : out) {
+		cout << item << endl;
+	}
+}
+
+//字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一个字母只会出现在其中的一个片段。返回一个表示每个字符串片段的长度的列表。
+//示例：
+//输入：S = "ababcbacadefegdehijhklij"
+//输出：[9, 7, 8]
+//解释：
+//划分结果为 "ababcbaca", "defegde", "hijhklij"。
+//每个字母最多出现在一个片段中。
+//输入：S = "abcadbcefghgehik"
+//输出：[7, 7, 1, 1]
+//解释：
+//划分结果为 "abcadbc", "efghgeh", "i", "k"。
+
+void test() {
+	string  input = "ababcbacadefegdehijhklij";
+
+	vector<int> result;
+	unordered_map<char, int> index_map;
+
+	for (int i = 0; i < input.size(); i++) {
+		index_map[input[i]] = i;
+	}
+	for (pair<char, int> item : index_map) {
+		cout << item.first << ':' << item.second << endl;
+	}
+	int start = 0, last = 0;
+	for (int i = 0; i < input.size(); i++) {
+		last = max(last, index_map[input[i]]);
+		if (i == last) {
+			result.push_back(last - start + 1);
+			start = i + 1;
+		}
+	}
+
+	for (int item : result) {
 		cout << item << endl;
 	}
 }
